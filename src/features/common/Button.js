7ce59e28f4d2df69bf9ab6337ecb0c83a97e106c
Dan <dan.arias@auth0.com>
2018-08-10 06:21:28 -0400

    Add three states to Button component

--- src/features/common/Button.js
+++ src/features/common/Button.js
@@ -1,5 +1,19 @@
 import React from "react";
 
-const Button = props => <div>{props.label}</div>;
+import "./Button.css";
+
+const Button = props => (
+  <div
+    className={`Button ${
+      props.active
+        ? props.fill
+          ? `active fill`
+          : `active no-fill`
+        : `disabled`
+    }`}
+  >
+    {props.label}
+  </div>
+);
 
 export default Button;
--- src/stories/index.js
+++ src/stories/index.js
@@ -2,6 +2,11 @@ import React from "react";
 import { storiesOf } from "@storybook/react";
 import Button from "../../src/features/common/Button";
 
-storiesOf("Button", module).add("with text", () => (
-  <Button label={`Continue`} />
-));
+storiesOf("Button", module)
+  .add("active with fill", () => (
+    <Button label={`continue`} fill={true} active={true} />
+  ))
+  .add("active with no fill", () => (
+    <Button label={`sign up`} fill={false} active={true} />
+  ))
+  .add("disabled", () => <Button label={`continue`} active={false} />);
